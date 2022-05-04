dec624a4b05bdb8c8728a20efbcddd4b4a5deac9
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2022-05-04 09:54:53 -0700

    rebuilt module

--- lib/PixelTextSettings.d.ts
+++ lib/PixelTextSettings.d.ts
@@ -3,12 +3,15 @@ import PixelFontFace from './PixelFontFace';
 export interface PixelTextSettings {
     fontFace: PixelFontFace;
     color: Color;
+    align: number;
+    vAlign: number;
     strokeColor: Color;
     scaleDownToPhysicalSize: boolean;
     screenSpace: boolean;
     pixelSizeInClipSpaceUniform?: Uniform;
     constantSizeOnScreen?: boolean;
     letterSpacing: number;
+    prescale: number;
 }
 export declare const pixelTextSettings: {
     generic: PixelTextSettings;
