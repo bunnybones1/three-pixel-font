48c80583258f1d624d247fb297760ce47d51ca80
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2022-05-22 20:53:20 -0700

    wip

--- esbuild-build-lib.js
+++ esbuild-build-lib.js
@@ -3,12 +3,12 @@ import { glsl } from "esbuild-plugin-glsl";
 import { nodeExternalsPlugin }  from 'esbuild-node-externals'
 
 build({
-    entryPoints: ['src/index.ts'],
+    entryPoints: ['src/index.ts', 'src/PixelTextSettings.ts', 'src/PixelFontFace.ts'],
     outdir: 'lib',
     bundle: true,
     sourcemap: true,
     minify: false,
-    splitting: false,
+    splitting: true,
     format: 'esm',
     target: ['esnext'],
     tsconfig: './tsconfig.module.json',
